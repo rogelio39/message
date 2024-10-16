2bf579b84f954597cebbe2d11d3e9d1ac654cebb
rogelio <andresrogesu@gmail.com>
2024-10-16 11:28:19 -0300

    Update Songs.jsx

--- message-love/src/components/Songs/Songs.jsx
+++ message-love/src/components/Songs/Songs.jsx
@@ -3,29 +3,17 @@ import './Songs.css'
 const Songs = () => {
     return (
         <div className="songs-container">
-            <h1>Canciones que me hacen pensarte o recordarte</h1>
-            <h2>Hoy amaneci y escuche esta cancion y pense en ti</h2>
-            <p>Pense en como tu amor me hace sentir que estoy en el cielo.
-                Como tus besos y tus brazos me dan la paz y seguridad que necesito.
-                En la forma en que tus ojos me miran me hacen sentir como si fuera el hombre mas bello del mundo.
-                Y en como con el paso de los años seguimos juntos, amandonos, disfrutando de los momentos buenos
-                y luchando cuando las cosas se ponen dificiles.
+            <h1>Hoy amaneci pensando en ti</h1>
+            <h2>Al despertar te vi en nuestra cama, dormida</h2>
+            <p>y no podia dejar de pensar en cuanto Te Amo. En lo feliz que me haces.
+                En lo feliz que soy de que tu seas feliz. Suena ironico, hasta gracioso, pero amo verte bien, feliz. Alegre.
+                Te Amo muchisimo sabes?
             </p>
 
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/Sc-AV2UqMXU?si=9vlOgrYl_196POA9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/JcamjK-uNa4?si=ztpeIP6ZELjWJKiW" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
 
-            <p>No olvides jamas cuanto Te Amo. Y que te amo en los momentos hermosos, bellos, faciles</p>
-            <h4>Pero te amo aun mas en esos momentos dificiles. Cuando no concuerdo contigo. O estoy enojado o tu lo estas conmigo.
-                Te Amo mas en esos momentos donde las cosas se ponen duras.
-                Te Amo ahi donde duele.
-                Donde incomoda.
-                y no pienso rendirme jamas.
-                Porque te lo prometi.
-                Separarme de ti no es una opcion.
-                No esta en la lista.
-                No existe.
-            </h4>
+            <p>No olvides jamas cuanto Te Amo</p>
             <h3>Te Amo mi infinito🥰❤️</h3>
         </div>
     )
