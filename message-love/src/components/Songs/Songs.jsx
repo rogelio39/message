76094fc1315001ee2543d3de216865a07f084a6e
rogelio <andresrogesu@gmail.com>
2024-10-18 10:00:30 -0300

    Update Songs.jsx

--- message-love/src/components/Songs/Songs.jsx
+++ message-love/src/components/Songs/Songs.jsx
@@ -3,14 +3,22 @@ import './Songs.css'
 const Songs = () => {
     return (
         <div className="songs-container">
-            <h1>Hoy amaneci pensando en ti</h1>
-            <h2>Al despertar te vi en nuestra cama, dormida</h2>
-            <p>y no podia dejar de pensar en cuanto Te Amo. En lo feliz que me haces.
-                En lo feliz que soy de que tu seas feliz. Suena ironico, hasta gracioso, pero amo verte bien, feliz. Alegre.
-                Te Amo muchisimo sabes?
+            <h1>Otro dia amaneciendo y pensando en ti</h1>
+            <p>Hoy al despertar pense en lo lejos que hemos llegado. En donde estamos hoy. En cuanto mejoramos para poder ser mejor para nosotros.
+                Y admito que me lleno el corazon de alegria. Me hizo sentir orgulloso de nosotros. De ti, de mi.
+            </p>
+            <p>
+                me senti feliz de muchas cosas en nuestra vida que tuvimos la dicha de que fueron momentos lindos y romanticos.
+                Gracias por todo lo que me brindas todos los dias.
+            </p>
+            <p>
+                Y, aunque me explayare mejor hoy cuando te vea, no queria dejar pasar el hecho, el hermoso y romantico hecho de que nos tienes de fondo
+                de pantalla, o no se que era, pero tienes una fotito de nosotros en tu celu❤️ Cuando vi eso mori de amor y volvi a vivir. Se me
+                desarmo el corazoncito, no se, parece algo tan simple pero para mi significo todo, muchisimo. Ame vernos ahi, de fondo en tu celu. Sos un amor!
+
             </p>
 
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/JcamjK-uNa4?si=ztpeIP6ZELjWJKiW" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/e1mOmdykmwI?si=FUJqrKThDvBIrpYr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
 
             <p>No olvides jamas cuanto Te Amo</p>
